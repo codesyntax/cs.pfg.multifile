59b00205857ee2bff5cb0224737870be0e9bf371
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-07-06 16:33:25 +0200

    rename readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='cs.pfg.multifile',
       version=version,
       description="PloneFormGen fields to allow uploading multiple files",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
